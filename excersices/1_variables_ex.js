b2155f275ac6b68e9d6822b326dc4b882df8f30e
hermanpapko <herman.papko@gmail.com>
2024-09-06 20:56:46 +0200

    Excercise 1 is done

--- excersices/1_variables_ex.js
+++ excersices/1_variables_ex.js
@@ -1,40 +1,40 @@
 //? what should be displayed?
 /************/
-let a = 10
-let b = 20
+let a = 10;
+let b = 20;
 
-console.log(a + b)
+console.log(a + b);
 /************/
-
+30;
 //! find, describe and fix a mistake
 /************/
-let 2_a = 1
+let a2 = 1;
 
-console.log(2_a)
+console.log(a2);
 /************/
-
+// Can not start with a number
 /************/
-const a3 = 1
-a3 = 2
-console.log(a3)
+const a3 = 1;
+console.log(a3);
 /************/
-
+// const can not be changed
 /************/
 function sum() {
-	let a4 = 1
-	let b4 = 2
-	console.log(a4 + b4)
+  let a4 = 1;
+  let b4 = 2;
+  return a4 + b4;
 }
 
-console.log(a4 + b4)
+console.log(sum());
 /************/
-
+// Displays function's result, not calculation result
 /************/
-let a5 = 1
-let b5 = 2
+let a5 = 1;
+let b5 = 2;
 
-function sum() {
-	console.log(a5 + b5)
+function sum2() {
+  console.log(a5 + b5);
 }
-sum()
+sum2();
 /************/
+// That's right
