4d548808d46cb271a9887b3b3261a76ee333ba67
hermanpapko <herman.papko@gmail.com>
2024-09-06 18:49:08 +0200

    functions (#1)
    Co-authored-by: apopko <apopko@roadie.com>

--- excersices/1_helloWorld.js
+++ excersices/1_helloWorld.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+// Performing a task
+function greet(name, lastName) {
+    console.log("Hello " + name + " " + lastName);
+}
+
+// Calculates a value
+function square(number) {
+    return number * number;
+}
+
+console.log(square(2));
+greet('Herman', 'Papko');
+
