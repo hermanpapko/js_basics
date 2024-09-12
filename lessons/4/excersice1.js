03411567ea80d66814b66d90f7272f5f60ed6e3d
hermanpapko <herman.papko@gmail.com>
2024-09-12 18:55:39 +0200

    Lesson (#6)
    * 4lesson
    
    * 4lesson
    
    * 4lessons
    
    * excercise3
    
    * excercise3

---
+++ lessons/4/excersice1.js
@@ -0,0 +1,11 @@
+function max(a1, a2) {
+  if (a1 > a2) {
+    console.log(a1);
+  }
+  if (a1 === a2) {
+    console.log("Numbers are the same");
+  } else {
+    console.log(a2);
+  }
+}
+max(4, 82);
