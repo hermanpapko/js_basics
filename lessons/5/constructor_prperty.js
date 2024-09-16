aa451128ab3e9ae7a7507ead8990b6b6b78e98b4
hermanpapko <herman.papko@gmail.com>
2024-09-16 18:22:44 +0200

    6

--- lessons/5/constructor_prperty.js
+++ lessons/5/constructor_prperty.js
@@ -1,1 +1,20 @@
-let x = {};
+// Factory Fumction
+function createCircle(radius) {
+  return {
+    radius,
+    draw() {
+      console.log("draw");
+    },
+  };
+}
+const circle = createCircle(1);
+console.log(circle);
+
+function Circle(radius) {
+  this.rasius = radius;
+  this.draw = function () {
+    console.log("draw");
+  };
+  return this;
+}
+const another = new Circle(1);
