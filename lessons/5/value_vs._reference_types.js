8f565fbed194a08f2187205cf513869c0aebbaf3
hermanpapko <herman.papko@gmail.com>
2024-09-18 21:05:14 +0200

    clear

---
+++ lessons/5/value_vs._reference_types.js
@@ -0,0 +1,17 @@
+let x = { value: 10};
+let y = x;
+
+x.value = 20;
+
+console.log(y)
+
+
+
+let obj = {value : 10};
+
+function increase(obj) {
+    obj.value++
+}
+
+increase(obj);
+console.log(obj);
